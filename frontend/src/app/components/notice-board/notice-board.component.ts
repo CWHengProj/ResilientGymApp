3cb095137db805ad4c0f6d10ad96e83d89ffaa6a
CWHengProj <142023227+CWHengProj@users.noreply.github.com.>
2025-09-14 00:51:46 +0800

    noticeboard for future usage

--- frontend/src/app/components/notice-board/notice-board.component.ts
+++ frontend/src/app/components/notice-board/notice-board.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './notice-board.component.css'
 })
 export class NoticeBoardComponent {
-
+  //should have a clickable button to shush the notifications
 }
